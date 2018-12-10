c052888665088cd8297ed4ef2caf899edae98a50
pjitthamma <patboke.jitthamma@agoda.com>
2018-12-10 15:32:17 +0700

    first commit

---
+++ HtmlCombine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HtmlCombine
+{
+    class Program
+    {
+        static void Main(string[] userInput)
+        {
+            RegressionReport init = new RegressionReport();
+            string report = null;
+
+            for (int i =0; i < userInput.Length; i++)
+            {
+                 report += userInput[i]+ " ";
+            }
+            string[] splitReport = report.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            init.GetBodyReport(splitReport);
+        }
+    }
+}
---
+++ HtmlCombine/RegressionReport.cs
@@ -0,0 +1,87 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace HtmlCombine
+{
+    class RegressionReport
+    {
+        public void GetBodyReport(string[] reportList)
+        {
+            string htmlText = null;
+            List<string> htmlBody = new List<string>();
+
+            //Get HTML body from each regression report
+            foreach (var report in reportList)
+            {
+                using (StreamReader readStream = new StreamReader(report+@"\Report.html"))
+                {
+                    Console.WriteLine("Report:" +report + @"\Report.html");
+                    htmlText = readStream.ReadToEnd();
+                }
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(htmlText);
+
+                var body = doc.DocumentNode.SelectSingleNode("(//div[@class='row'])[3]");
+                if (body != null)
+                {
+                    //get only body
+                    htmlBody.Add(body.OuterHtml);
+                }
+            }
+            CombineReport(htmlBody, reportList[0]);
+        }
+
+        public void CombineReport(List<string> report, string finalFolder)
+        {
+            string fullReport = null;
+
+            //Get Templete
+            string ReportHeader = GetReportPath(@"\ReportTemplete\header.html");
+            string ReportFooter = GetReportPath(@"\ReportTemplete\footer.html");
+
+            //Add header
+            using (StreamReader readStream = new StreamReader(ReportHeader + @"\ReportTemplete\header.html"))
+            {
+                fullReport = readStream.ReadToEnd();
+            }
+            //Add Body
+            foreach (var item in report)
+            {
+                fullReport += item;
+            }
+            //Add Footer
+            using (StreamReader readStream = new StreamReader(ReportFooter + @"\ReportTemplete\footer.html"))
+            {
+                fullReport += readStream.ReadToEnd();
+            }
+
+            //Creat new report
+            using (FileStream fs = new FileStream(finalFolder + @"\ReportFinal.html", FileMode.Create))
+            {
+                using (StreamWriter w = new StreamWriter(fs))
+                {
+                    w.WriteLine(fullReport);
+                }
+            }
+        }
+
+        public string GetReportPath(string templete)
+        {
+            string _path = null;
+            var file = new FileInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            //var parentDirectory = file.Directory?.Parent;
+
+            //if (parentDirectory != null)
+            //{
+            //    _path = Path.Combine(parentDirectory.FullName, templete);
+            //}
+
+            return file.ToString();
+        }
+    }
+}
